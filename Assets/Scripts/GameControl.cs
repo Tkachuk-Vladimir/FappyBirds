d2326a50a9211ae1482bc5f6973af52bf5aae5cf
vovatk <tck.vova@ya.ru>
2021-09-22 21:59:49 +0300

    add all files in git

---
+++ Assets/Scripts/GameControl.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameControl : MonoBehaviour
+{
+    
+    public GameObject gameOverText;
+    public bool gameOver = false;
+    public static GameControl instance;
+    public float scrollSpeed = -1.5f;
+    public Text scoreText;
+
+    private int score = 0;
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+           instance = this;
+        }
+        else if( instance != this)
+                 {
+                   Destroy(gameObject);
+                 }
+    }
+
+    void Update()
+    {
+      if (gameOver == true && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+        }
+    }
+
+    // эта функция прибавлят плюс одно очко
+    public void BirdScored()
+    {
+       if(gameOver == true)// проверка жива ли птичка
+        {
+            return;
+        }
+        score++; // за слешаи - это коменты,  score = score + 1;
+        scoreText.text = "Score: " + score.ToString(); // выводим очки на экран, пользуемся ui текстом
+    } 
+
+
+    public void BirdDied()
+    {
+        gameOver = true;
+        gameOverText.SetActive (true);
+    }
+}
---
+++ Assets/Scripts/columnPool.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class columnPool : MonoBehaviour
+{
+    public int columnPoolSize = 5;
+    public GameObject columnPrefab;
+    public float spawnRate = 4f;
+    public float columnMin = -1f;
+    public float columnMax = 3f;
+
+
+    private GameObject[] columns;
+    private Vector2 objectPoolPosition = new Vector2(-15f, -25f);
+    private float timeLastSpawned = 0f;
+    private float spawnXPosition = 10f;
+    private int currentColumn = 0;
+
+    void Start()
+    {
+        columns = new GameObject[columnPoolSize];
+        for (int i = 0; i < columnPoolSize; i++)
+        {
+            columns[i] = (GameObject)Instantiate(columnPrefab, objectPoolPosition, Quaternion.identity);
+        }
+    }
+    
+    void Update()
+    {
+        timeLastSpawned += Time.deltaTime;
+
+        if(GameControl.instance.gameOver == false && timeLastSpawned >= spawnRate)
+        {
+            timeLastSpawned = 0f;
+            float spawnYPosition = Random.Range(columnMin, columnMax);
+            columns[currentColumn].transform.position = new Vector2(spawnXPosition, spawnYPosition);
+            currentColumn++;
+            if (currentColumn >= columnPoolSize)
+            {
+                currentColumn = 0;
+            }
+        }
+    }
+}
